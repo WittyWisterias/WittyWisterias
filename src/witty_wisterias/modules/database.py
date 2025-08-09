3f5fee944f9cb33150831be36b7cd1593fe9ccfb
Vinyzu <50874994+Vinyzu@users.noreply.github.com>
2025-08-09 11:54:32 +0200

    Add Query to search for latest Data in
    Fixes https://github.com/WittyWisterias/WittyWisterias/issues/8
    
    Fixes https://github.com/WittyWisterias/WittyWisterias/issues/9

--- src/witty_wisterias/modules/database.py
+++ src/witty_wisterias/modules/database.py
@@ -8,6 +8,7 @@ from io import BytesIO
 # Using httpx instead of requests as it is more modern and has built-in typing support
 import httpx
 import xxhash
+from bs4 import BeautifulSoup
 from PIL import Image
 
 from .exceptions import InvalidResponseError
@@ -23,6 +24,23 @@ class Database:
 
     def __init__(self) -> None:
         self.session = httpx.Client()
+
+    @staticmethod
+    def extract_timestamp(url: str) -> float:
+        """
+        Extracts the timestamp from the Filename of a given URL in our format.
+
+        Args:
+            url (str): The URL from which to extract the timestamp.
+
+        Returns:
+            float: The extracted timestamp as a float.
+        """
+        # Use regex to find the timestamp in the URL
+        match = re.search(r"(\d+\.\d+)", url)
+        if match:
+            return float(match.group(1))
+        return 0.0
 
     @staticmethod
     def base64_to_image(base64_data: str) -> bytes:
@@ -65,7 +83,7 @@ class Database:
             str: The auth token required for uploading images.
 
         Raises:
-            AssertionError: If the configuration data cannot be fetched or the auth token is not found.
+            InvalidResponseError: If the configuration data cannot be fetched or the auth token is not found.
         """
         # Getting necessary configuration data for upload
         config_response = self.session.get("https://freeimghost.net/upload")
@@ -89,7 +107,7 @@ class Database:
             image_bytes (bytes): The image bytes to upload.
 
         Raises:
-            AssertionError: If the upload fails or the response is not as expected.
+            InvalidResponseError: If the upload fails or the response is not as expected.
         """
         # Get current UTC time
         utc_time = datetime.now(UTC)
@@ -120,6 +138,53 @@ class Database:
         if response.status_code != 200:
             raise InvalidResponseError("Failed to upload image to the image hosting service.")
 
+    def query_data(self) -> str:
+        """
+        Queries the latest data from the database.
+
+        Returns:
+            str: The latest data.
+
+        Raises:
+            InvalidResponseError: If the query fails or the response is not as expected.
+        """
+        # Query all images with the search term "WittyWisterias" from the image hosting service
+        response = self.session.get("https://freeimghost.net/search/images/?q=WittyWisterias")
+        # Check if the response is successful
+        if response.status_code != 200:
+            raise InvalidResponseError("Failed to query latest image from the image hosting service.")
+
+        # Extracting the latest image URL from the response using beautifulsoup
+        soup = BeautifulSoup(response.text, "html.parser")
+        # Find all image elements which are hosted on the image hosting service
+        image_links = [img.get("src") for img in soup.find_all("img") if "https://freeimghost.net/" in img.get("src")]
+
+        # Sort the image elements by the timestamp in the filename (in the link) (newest first)
+        sorted_image_links = sorted(image_links, key=self.extract_timestamp, reverse=True)
+
+        # Find the first image link that contains our validation header and return its pixel byte data
+        for image_link in sorted_image_links:
+            # Fetch the image content
+            image_content = self.session.get(image_link).content
+
+            # Get the byte content of the image without the PNG Image File Header
+            image_stream = BytesIO(image_content)
+            pil_image = Image.open(image_stream).convert("RGB")
+            pixel_byte_data = pil_image.tobytes()
+
+            # Validate the image content starts with our validation header
+            if pixel_byte_data.startswith(b"WittyWisterias"):
+                # Remove the validation header and noise bytes from the first valid image
+                no_header_data = pixel_byte_data[len(b"WittyWisterias") + 8 :]
+                # Remove any padding bytes (if any) to get the original data
+                no_padding_data = no_header_data.rstrip(b"\x00")
+                # Decode bytes into string and return it
+                decoded_data: str = no_padding_data.decode("utf-8", errors="ignore")
+                return decoded_data
+
+        # If no valid image is found, raise an error
+        raise InvalidResponseError("No valid image found in the response.")
+
     def upload_data(self, base64_data: str) -> None:
         """
         Uploads base64 encoded data as an image to the database hosted on the Image Hosting Service.
@@ -128,7 +193,7 @@ class Database:
             base64_data (str): The base64 encoded data to upload.
 
         Raises:
-            AssertionError: If the upload fails or the response is not as expected.
+            InvalidResponseError: If the upload fails or the response is not as expected.
         """
         image_bytes = self.base64_to_image(base64_data)
         self.upload_image(image_bytes)
@@ -138,5 +203,6 @@ if __name__ == "__main__":
     # Example/Testing usage
     db = Database()
     # Base64 for "Hello, World!"
-    base64_string = "SGVsbG8sIFdvcmxkIQ=="
+    base64_string = base64.b64encode(b"Hello, World! Witty Wisterias.").decode("utf-8")
     db.upload_data(base64_string)
+    print(db.query_data())
