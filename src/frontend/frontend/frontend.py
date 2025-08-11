ad86f1b1cb1d90c98dafad35b78431f5a1d0a5a4
Ben Gilbert <bensgilbert@outlook.com>
2025-08-11 13:43:59 +0100

    add send image dialog

--- src/frontend/frontend/frontend.py
+++ src/frontend/frontend/frontend.py
@@ -5,6 +5,43 @@ class State(rx.State):
     """The app state."""
 
 
+def send_image_dialog() -> rx.Component:
+    """The dialog (and button) for sending an image"""
+    return rx.dialog.root(
+        rx.dialog.trigger(
+            rx.button(
+                rx.center(rx.text("Send Image")),
+                padding="24px",
+                radius="large",
+            ),
+        ),
+        rx.dialog.content(
+            rx.dialog.title("Send Image"),
+            rx.dialog.description(
+                "Send an image by describing it in the box below.",
+                size="2",
+                margin_bottom="16px",
+            ),
+            rx.text_area(placeholder="Describe it here..."),
+            rx.flex(
+                rx.dialog.close(
+                    rx.button(
+                        "Cancel",
+                        color_scheme="gray",
+                        variant="soft",
+                    ),
+                ),
+                rx.dialog.close(
+                    rx.button("Send"),
+                ),
+                spacing="3",
+                margin_top="16px",
+                justify="end",
+            ),
+        ),
+    )
+
+
 def index() -> rx.Component:
     """Welcome Page (Index)"""
     return rx.container(
@@ -24,11 +61,7 @@ def index() -> rx.Component:
                     padding="24px",
                     radius="large",
                 ),
-                rx.button(
-                    rx.center(rx.text("Send Image")),
-                    padding="24px",
-                    radius="large",
-                ),
+                send_image_dialog(),
                 class_name="w-full",
                 columns="2",
                 spacing="4",
